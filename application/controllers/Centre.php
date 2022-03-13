ea9a77ff6f4a91cd2adabe70645d30d4f265e2ad
Princia08 <80781644+Princia08@users.noreply.github.com>
2022-03-13 12:24:30 +0300

    Controller

---
+++ application/controllers/Centre.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Centre extends CI_Controller {
+
+}
---
+++ application/controllers/Reservation.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Reservation extends CI_Controller {
+
+}
