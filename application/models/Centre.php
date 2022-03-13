3b6bd9ff17a830356b6582ac6c52b5ddeb1a2c8e
Princia08 <80781644+Princia08@users.noreply.github.com>
2022-03-13 20:46:29 +0300

    commity

--- application/models/Centre.php
+++ application/models/Centre.php
@@ -2,5 +2,18 @@
 
 class Centre extends CI_Model
 {
+    public function getAllCentre()
+    {
+        $retour = array();
+        $sql = "select * from Centre";
+        $query = $this->db->query($sql);
+        $i = 0;
+        foreach($query -> result_array() as $row)
+        {
+            $retour[$i] = $row;
+            $i++;
+        }
+        return $retour;
+    }
 }
 ?>
