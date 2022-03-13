026cb6f83958f3cd76bdc995974cc80bfac8034a
syraxxxx <rsitraka181@gmail.com>
2022-03-13 18:18:36 +0300

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -61,12 +61,12 @@
             <div class="menu_section">
               <h3>General</h3>
               <ul class="nav side-menu">
-                <li><a><i class="fa fa-home"></i> Statistiques <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-bar-chart-o"></i> Statistiques <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
                     <li><a href="index.html">Dashboard</a></li>
                   </ul>
                 </li>
-                <li><a><i class="fa fa-home"></i> Reservation <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-edit"></i> Reservation <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
                     <li><a href="index.html">Dashboard</a></li>
                   </ul>
