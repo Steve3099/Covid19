a586c347dd6634e8a02163162d55620f45a4b2e9
Junx2001 <ratsirarsonj@gmail.com>
2022-03-14 07:14:06 +0300

    p

--- application/controllers/StatistiqueController.php
+++ application/controllers/StatistiqueController.php
@@ -11,6 +11,7 @@ class StatistiqueController extends CI_Controller {
         $data['vacMort'] =$this->Statistique->getNombreVaccineMaty();
         $data['vacMarary'] =$this->Statistique->getNombreMararyVaccine();
         $data['vacParMois'] =$this->Statistique->getNombreVaccinParMois();
+        $data['vacParResultat'] =$this->Statistique->getNombreResultatVaccination();
         $data['view'] ='stat';
         //var_dump($data['vacMarary']);
         $this->load->view('template',$data);
--- application/views/stat.php
+++ application/views/stat.php
@@ -108,84 +108,57 @@ const val = [];
             </div>
         </div>
 
-        <div class="col-md-4 col-sm-4 ">
-            <div class="x_panel tile fixed_height_320 overflow_hidden">
-                <div class="x_title">
-                    <h2>Device Usage</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                        <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                        </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">Settings 1</a>
-                                <a class="dropdown-item" href="#">Settings 2</a>
-                            </div>
-                        </li>
-                        <li><a class="close-link"><i class="fa fa-close"></i></a>
-                        </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="x_content">
-                    <table class="" style="width:100%">
-                        <tr>
-                            <th style="width:37%;">
-                                <p>Top 5</p>
-                            </th>
-                            <th>
-                                <div class="col-lg-7 col-md-7 col-sm-7 ">
-                                    <p class="">Device</p>
-                                </div>
-                                <div class="col-lg-5 col-md-5 col-sm-5 ">
-                                    <p class="">Progress</p>
-                                </div>
-                            </th>
-                        </tr>
-                        <tr>
-                            <td>
-                                <canvas class="canvasDoughnut" height="140" width="140" style="margin: 15px 10px 10px 0"></canvas>
-                            </td>
-                            <td>
-                                <table class="tile_info">
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square blue"></i>IOS </p>
-                                        </td>
-                                        <td>75%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square green"></i>Android </p>
-                                        </td>
-                                        <td>4%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square purple"></i>Blackberry </p>
-                                        </td>
-                                        <td>20%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square aero"></i>Symbian </p>
-                                        </td>
-                                        <td>15%</td>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            <p><i class="fa fa-square red"></i>Others </p>
-                                        </td>
-                                        <td>30%</td>
-                                    </tr>
-                                </table>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
-            </div>
+        <div style="width:500px">
+        <canvas id="myChart2" width="400" height="400"></canvas>
         </div>
-    
+
+
+    <br />
+
+    <script>
+
+
+const tab2 = [];
+
+const val2 = [];
+
+
+<?php foreach($vacParResultat as $el){ ?>
+    tab2.push('<?php echo $el['resultat']; ?>')
+ <?php } ?>
+      
+<?php foreach($vacParResultat as $el){ ?>
+    val2.push(<?php echo $el['nb']; ?>)
+ <?php } ?>
+      
+        const data2 = {
+          labels: tab2,
+          datasets: [{
+            label: 'Nombre par Résultat Vaccination',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: val2,
+          }]
+        };
+      
+        const config2 = {
+  type: 'bar',
+  data: data2,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  },
+};
+</script>
+<script>
+    const myChart2 = new Chart(
+      document.getElementById('myChart2'),
+      config2
+    );
+  </script>
 
         </div>
 
